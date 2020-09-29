b0e73779e54468a5c094640e727fb93619c613b9
grrazzlle <cel47@miami.edu>
2020-09-29 00:02:08 -0400

    heroku test

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,23 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
-  host: 'localhost',
+// const connection = mysql.createConnection({
+//   host: 'localhost',
+//   port: 3306,
+//   user: 'root',
+//   password: 'root',
+//   database: 'burgers_db',
+// });
+
+
+connection = mysql.createConnection({
+  host: 'sm9j2j5q6c8bpgyq.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	',
   port: 3306,
-  user: 'root',
-  password: 'root',
-  database: 'burgers_db',
+  user: 'hxcvplk4x5wsgvdw',
+  password: 'l9p980bq552xug63',
+  database: 'qylv20h1zbug7zsa',
 });
+
 
 // Make connection.
 connection.connect(function(err) {
